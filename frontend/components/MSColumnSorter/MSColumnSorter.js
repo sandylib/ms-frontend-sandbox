9fcbad6633017b60d5d19e15d6c5afe7f5620428
Sandy <s110557@192-168-1-100.tpgi.com.au>
2019-01-24 20:46:23 +1100

    fully functional components

--- frontend/components/MSColumnSorter/MSColumnSorter.js
+++ frontend/components/MSColumnSorter/MSColumnSorter.js
@@ -9,113 +9,45 @@ const ICONS = {
  desc: <FaSortDown />
 };
 
-// Rotate through sorting order
-const SORT_ORDER = ['default', 'asc', 'desc'];
-const getNextOrder = (current = 'default') => {
- const currentIndex = SORT_ORDER.findIndex(s => s === current);
- const newIndex = (currentIndex + 1) % SORT_ORDER.length;
- return SORT_ORDER[newIndex];
-};
 
-const HeaderLabel = ({ children = 'label', order = 'default', sortable = false, selectedCol }) => {
+const HeaderLabel = ({ children = 'label', direction = 'default', sortable = false }) => {
   if(sortable) {
-    order = selectedCol ? order : 'default';
-   return (<div>{children} {ICONS[order]} </div>)
+   return (<div>{children} {ICONS[direction]} </div>)
   }
   else
     return (<div>{children}</div>)
 }
 
+   // Binds all the events
+const addTransforms = (columns, handleSortingColumns, sortingColumns) => {
+  return columns.map(column => {
+    return {
+      ...column,
+      header: {
+        ...column.header,
+        transforms: [
+          (label, props) => ({
+            onClick: (e) => props.column.sortable ?  handleSortingColumns(props.column.property, e.shiftKey)  : null, //this.sortColumn(props.column.property, e, rows)
+            children: (
+              <HeaderLabel direction={sortingColumns[label] ? sortingColumns[label].direction : null} sortable={props.column.sortable} >
+                <div>{label}</div>
+              </HeaderLabel>
+            )
+          })
+        ]
+      }
+    };
+   });
+}
 
 export default class MSColumnSorter extends React.Component {
- constructor(props) {
-   super(props);
-
-   // Similar to _orderBy
-   this.doSort = (items, property, order) => {
-     switch (order) {
-       case 'asc':
-         return [
-           ...items.sort((a, b) => {
-             return a[property] > b[property] ? 1 : -1;
-           })
-         ];
-       case 'desc':
-         return [
-           ...items.sort((a, b) => {
-             return a[property] < b[property] ? 1 : -1;
-           })
-         ];
-       default:
-         return [...items];
-     }
-   };
-
-   this.sortColumn = (property, e, originalRows) => { // code
-
-     const newColumns = [...this.state.columns];
-
-     let newRows = [...this.state.rows]; // this have not use yet
-     // Find the target column we want to sort
-     const index = newColumns.findIndex(col => col.property === property);
-     const targetColumn = newColumns[index];
-
-     const newOrder = getNextOrder(newColumns[index].header.order);
-     newColumns[index].header.order = getNextOrder(
-       newColumns[index].header.order
-     );
-
-     if(e.shiftKey) { //TODO
-      // console.log('key', e) //
-      // newRows = this.doSort(newRows, property, newOrder);
-    }else {
-      newRows = this.doSort(originalRows, property, newOrder); 
-    }     
-     debugger;
-
-     let targetLable = e.shiftKey ?  targetColumn.header.label : ''; // will be target lables
-    
-     this.setState({ columns: addTransforms(newColumns, newRows, targetLable), rows: newRows });
-   };
-
-
-
-   // Binds all the events
-   const addTransforms = (columns, rows, selectedCol) =>
-     columns.map(column => {
-       return {
-         ...column,
-         header: {
-           ...column.header,
-           transforms: [
-             (label, props) => ({
-               onClick: (e) => props.column.sortable ?  this.sortColumn(props.column.property, e, rows) : null, 
-               children: (
-                 <HeaderLabel order={props.column.header.order} sortable={props.column.sortable} selectedCol={selectedCol === label} >
-                   {label}
-                 </HeaderLabel>
-               )
-             })
-           ]
-         }
-       };
-     });
-
-   this.state = {
-     columns: addTransforms(this.props.columns, this.props.rows),
-     rows: [...this.props.rows]
-   };
- }
-
  render() {
    return (
      <Table.Provider
        className="table table-striped table-bordered"
-       columns={this.state.columns}
-     >
-
-<Table.Header />
-       <Table.Body rows={this.state.rows} rowKey="id" />
+       columns={addTransforms(this.props.columns, this.props.handleSortingColumns, this.props.sortingColumns)}>
+      <Table.Header />
+       <Table.Body rows={this.props.rows} rowKey="id" />
      </Table.Provider>
    );
  }
--- frontend/components/Sample/Sample.js
+++ frontend/components/Sample/Sample.js
@@ -57,21 +57,130 @@ const columns = [
 ];
 
 const rows = [
-  { id: 1, name: 'jack', family: 'hanson', city: 'sydney', score: 100 }, 
+  { id: 1, name: 'jack', family: 'hanson', city: 'sydney', score: 600 }, 
   { id: 2, name: 'peter', family: 'street', city: 'melbourne', score: 200 }, 
   { id: 3, name: 'joe', family: 'larson', city: 'brisbane', score: 300 }, 
   { id: 4, name: 'simon', family: 'long', city: 'perth', score: 400 }, 
   { id: 5, name: 'abraham', family: 'blue', city: 'darwin', score: 500 } 
 ];
 
+function doSort(items, property, direction) {
+  switch (direction) {
+    case 'asc':
+      return [
+        ...items.sort((a, b) => {
+          return a[property] > b[property] ? 1 : -1;
+        })
+      ];
+    case 'desc':
+      return [
+        ...items.sort((a, b) => {
+          return a[property] < b[property] ? 1 : -1;
+        })
+      ];
+    default:
+      return [...items];
+  }
+};
 
+const defaultSortingColumns = columns
+  .filter(column => column.sortable)
+  .reduce((accumulator, column) => ({ ...accumulator,
+    [column.property]: {
+      priority: null,
+      direction: 'default', // (column.property === 'name') ? 'asc' : 
+    }
+  }), {});
 
 export class Sample extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      sortingColumns: defaultSortingColumns
     };
+    this.getNextDirection = this.getNextDirection.bind(this);
+    this.getSortedRows = this.getSortedRows.bind(this);
+    this.handleSortingColumns = this.handleSortingColumns.bind(this);
+    this.getLowestExistingPriority = this.getLowestExistingPriority.bind(this);
+  }
+
+  getNextDirection(direction) {
+    const SORT_ORDER = ['default', 'asc', 'desc'];
+    const currentIndex = SORT_ORDER.findIndex(s => s === direction);
+    const newIndex = (currentIndex + 1) % SORT_ORDER.length;
+    return SORT_ORDER[newIndex];
+  };
+
+  getSortedRows(rows, sortingColumns) {
+    let sortedRows = [...rows];
+    const sortingColumnsWithPriorities = Object.keys(sortingColumns).filter(name => sortingColumns[name].priority !== null);
+
+    if (sortingColumnsWithPriorities.length) {
+      const orderedPropertyNames = sortingColumnsWithPriorities
+        .sort((name1, name2) => {
+          const priority1 = sortingColumns[name1].priority;
+          const priority2 = sortingColumns[name2].priority;
+          return priority1 < priority2 ? 1 : -1;
+        });
+        
+        orderedPropertyNames.forEach(propertyName => {
+          sortedRows = doSort(sortedRows, propertyName, sortingColumns[propertyName].direction)
+        });
+    } else { 
+      Object.keys(sortingColumns).forEach(propertyName => {
+        sortedRows = doSort(sortedRows, propertyName, sortingColumns[propertyName].direction)
+      });
+    }
+
+    return sortedRows;
+  }
+
+   getLowestExistingPriority () {
+    const priorityPropertyNames = Object.keys(this.state.sortingColumns).filter(propertyName => this.state.sortingColumns[propertyName].priority !== null)
+    if (!priorityPropertyNames.length) return null;
+    let lowestPriority = 0;
+    
+    priorityPropertyNames.forEach(propertyName => {
+      const priority = this.state.sortingColumns[propertyName].priority;
+      
+      if (priority > lowestPriority) lowestPriority = priority;
+    })
+    return lowestPriority;
+  }
+
+  handleSortingColumns(propertyName, isShiftKey)  {
+    let shiftKeySortingColumns, updatedPriority;
+    let normalClickSortingColumns = this.state.sortingColumns;
+    const propertyState = this.state.sortingColumns[propertyName];
+    if (isShiftKey) {
+      if (propertyState.priority !== null) {
+        const updatedSortingColumns = {
+          ...this.state.sortingColumns,
+          [propertyName]: {
+            ...this.state.sortingColumns[propertyName], priority: null
+          }
+        }
+        return this.setState({sortingColumns: updatedSortingColumns })
+      }
+      const lowestPriority = this.getLowestExistingPriority();
+  
+      if (lowestPriority === null) {
+        updatedPriority = 1;
+      } else {
+          updatedPriority = lowestPriority + 1; // make it next lowest priority
+      }
+
+      const nextDirection = this.getNextDirection(propertyState.direction);
+      shiftKeySortingColumns =  { ...this.state.sortingColumns, [propertyName]: { priority: updatedPriority, direction: nextDirection }}
+    } else {
+      updatedPriority = null;
+      const nextDirection = this.getNextDirection(propertyState.direction);
+      normalClickSortingColumns = { ...defaultSortingColumns, [propertyName]: { priority: null, direction: nextDirection } }
+   }
+
+   const updatedSortingColumns = isShiftKey ? shiftKeySortingColumns : normalClickSortingColumns;
+   this.setState({ sortingColumns: updatedSortingColumns });
   }
 
 
@@ -100,7 +209,11 @@ export class Sample extends React.Component {
             <div className="col-xs-12">
               {/* <MyTable /> */}
 
-              <MSColumnSorter columns={ columns } rows= {rows}  />
+              <MSColumnSorter 
+               columns={ columns } 
+               rows= {this.getSortedRows(rows, this.state.sortingColumns)}
+               sortingColumns={ this.state.sortingColumns }
+               handleSortingColumns={this.handleSortingColumns} />
             </div>
           </div>
         </div>
